6111174f8cf8f10bba7a91fb369218195ed3b0fd
Tyiasd <tyiasderp@gmail.com>
2017-12-25 23:30:31 +0100

    Add files via upload

--- Teslic.java
+++ Teslic.java
@@ -32,20 +32,66 @@ import javafx.stage.Stage;
  */
 public class Teslic extends Application {
     
+    public double round(double p){
+        p = Math.round(p * 100);
+        p = p/100;
+        return p;
+    }
+    
+    public int oob(int p, int min, int max){
+        if(p<min){p=min;}
+        if(p>max){p=max;}
+        return p;
+    }
+    
+    public void bold(int[] xs, int[] ys, int width, int height, int dots, PixelWriter writer, Color black){
+        for (int i = 0; i < dots; i++) {
+            for (int y = -1; y < 2; y++) {
+                for (int x = -1; x < 2; x++) {
+                    writer.setColor(oob(xs[i]+x, 0, width-1), oob(ys[i]+y, 0, height-1), black);
+                }
+            }
+        }
+    }
+    
     @Override
     public void start(Stage primaryStage) throws MalformedURLException {
+        
         //define the colour black
         Image c = new Image(new File("C:\\Users\\komp\\Documents\\NetBeansProjects\\teslic\\src\\teslic\\c.png").toURI().toURL().toExternalForm());
         PixelReader rc = c.getPixelReader();
         Color black = rc.getColor(0, 0);
+        Color red = rc.getColor(2, 0);
         
-        //get image with dots and make empty writable image of same size
-        Image srcIm = new Image(new File("C:\\Users\\komp\\Documents\\NetBeansProjects\\teslic\\src\\teslic\\test.jpg").toURI().toURL().toExternalForm());
-        int width = (int)srcIm.getWidth();
-        int height = (int)srcIm.getHeight();
-        PixelReader reader = srcIm.getPixelReader();
+        //get image of yug and make writable image of same size and content
+        Image srcIm = new Image(new File("C:\\Users\\komp\\Documents\\NetBeansProjects\\teslic\\src\\teslic\\redyu.png").toURI().toURL().toExternalForm());
+        int width = 1023;
+        int height = 869;
+        PixelReader srcImreader = srcIm.getPixelReader();
         WritableImage dest = new WritableImage(width, height);
+        PixelReader reader = dest.getPixelReader();
         PixelWriter writer = dest.getPixelWriter();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color color = srcImreader.getColor(x, y);
+                writer.setColor(x,y,color);
+            }
+        }
+        
+        //color 20 rand dots
+//        double randx;
+//        double randy;
+//        for (int i = 0; i < 20; i++) {
+//            randx = Math.random()*width;
+//            randy = Math.random()*height;
+//            if(srcImreader.getColor((int) randx, (int) randy).equals(red)){
+//                    writer.setColor((int) Math.floor(Math.random() * width),(int) Math.floor(Math.random() * height), black);
+//            }
+//            else
+//            {
+//                i=i--;
+//            }
+//        }
         
         //count dots
         int dots = 0;
@@ -105,16 +151,74 @@ outerloop :     for (int a = 0; a < 1; a++){
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 if (reader.getColor(x, y).equals(black)){break;}
-                writer.setColor(x, y, randboje[dotkojijenajblizi[y*width+x]]);
+                if(reader.getColor(x, y).equals(red))writer.setColor(x, y, randboje[dotkojijenajblizi[y*width+x]]);
             }
         }   
         for (int x = 0; x < width; x++) {
             for (int y = height-1; y > 0; y--) {
                 if (reader.getColor(x, y).equals(black)){break;}
-                writer.setColor(x, y, randboje[dotkojijenajblizi[y*width+x]]);
+                if(reader.getColor(x, y).equals(red))writer.setColor(x, y, randboje[dotkojijenajblizi[y*width+x]]);
+            }
+        }
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (reader.getColor(x, y).equals(black)){break;}
+                if(reader.getColor(x, y).equals(red))writer.setColor(x, y, randboje[dotkojijenajblizi[y*width+x]]);
             }
         }
         
+        //enlargen cities
+        bold(xs, ys, width, height, dots, writer, black);
+        
+        //color borders
+        Color ovaboja;
+        Color onaboja;
+        Color granica;
+        granica = rc.getColor(1, 0);
+        for (int i = 0; i < width; i++) {
+            ass:for (int j = 0; j < height; j++) {
+                ovaboja = reader.getColor(i, j);
+                for (int x = -1; x < 2; x++) {
+                    for (int y = -1; y < 2; y++) {
+                        onaboja = reader.getColor(oob(i+x, 0, width-1), oob(j+y, 0, height-1));
+                        if(!ovaboja.equals(onaboja) && !onaboja.equals(granica) && !ovaboja.equals(granica) && !ovaboja.equals(black) && !onaboja.equals(black)){
+                            writer.setColor(i, j, granica);
+                            //break ass;
+                            //onaboja = Color.color(Math.random(), Math.random(), Math.random());
+                        }
+                    }
+                }
+            }
+        }
+        
+        //remember cities
+        int brs = 0;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color color = reader.getColor(x, y);
+                if (color.equals(granica)){
+                    brs++;
+                }
+            }
+        }
+        int[] xb = new int[brs];
+        int[] yb = new int[brs];
+        int bordersfound = 0;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color color = reader.getColor(x, y);
+                if (color.equals(granica)){
+                    xb[bordersfound] = x;
+                    yb[bordersfound] = y;
+                    bordersfound++;
+                }
+            }
+        }
+        
+        //enlargen borders
+        bold(xb, yb, width, height, brs, writer, granica);
+        
+        //scene
         ImageView iv = new ImageView();
         iv.setImage(dest);
         AnchorPane root = new AnchorPane();
